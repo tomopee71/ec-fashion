43f797798eebdb49f070ccb2619699435b5eacad
tomopee71 <possiblevigour@gmail.com>
2026-01-03 10:10:05 +0900

    Product,index.css_change2

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,58 +1,83 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext.jsx";
-
-
+import { assets } from "../assets/assets.js";
 
 function Product() {
-
-  const {productId} = useParams();
-  const {products} = useContext(ShopContext);
-  const [productDate, setProductData] = useState(false)
-  const [image, setImage] = useState("")
-
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
-      if(item._id === productId) {
-      setProductData (item);
-      setImage(item.image[0])
-      return null;
-
-    }
-    
-    })
-
-  }
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  };
 
   useEffect(() => {
     fetchProductData();
-  },[productId, products])
+  }, [productId, products]);
 
-console.log(productId);
+  console.log(productId);
 
-  return productDate ? (
+  return productData ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
-{/* Product Data*/}
-<div className="flex gap-12 sm:gap-13 flex-col sm:flex-row">
-  {/* Product Images */}
-  <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
-    <div className="flex sm:flex-col oberflow-x-auto sm:overflow-y-scroll justify-between sm:justify-narmal sm:w-[18.7%] w-full">
-      {
-      productDate.image.map((item, index) => (
-
-        <img src={item} key={index} className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"alt="" />
-        
-      ))
-      }
-        
+      {/* Product Data*/}
+      <div className="flex gap-12 sm:gap-13 flex-col sm:flex-row">
+        {/* Product Images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                alt=""
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} alt="" className="w-full h-auto" />
+          </div>
+        </div>
+        {/* Product Details */}
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_icon} alt="" className="w-3" />
+            <img src={assets.star_dull_icon} alt="" className="w-3" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button key={index}>{item}</button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  
-  </div>
-
-    </div>
-  ) : <div className = "opacity-0">Loading...</div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 }
 
-export default Product
+export default Product;
