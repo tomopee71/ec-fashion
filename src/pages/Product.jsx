4393ef1df64430e9d2ab6d5b7052cae03112cc90
tomopee71 <possiblevigour@gmail.com>
2026-01-04 07:54:10 +0900

    RelatedProducts_add

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext.jsx';
 import { assets } from '../assets/assets.js';
+import RelatedProducts from '../components/RelatedProducts.jsx';
 
 function Product() {
   const { productId } = useParams();
@@ -82,7 +83,7 @@ function Product() {
           <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
             ADD TO CART
           </button>
-          <hr className="mt-8 sm:w-4/5" />
+          <hr className="border border-gray-300 mt-8 sm:w-4/5" />
           <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
             <p>100% Original product.</p>
             <p>Cash on delivery is available on this product.</p>
@@ -93,16 +94,17 @@ function Product() {
               {/* Description & Review Section */}
       <div className='mt-20'>
         <div className='flex'>
-          <b className='border px-5 py-3 text-sm'>Description</b>
-          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+          <b className='border border-gray-300 px-5 py-3 text-sm'>Description</b>
+          <p className='border border-gray-300 px-5 py-3 text-sm'>Reviews (122)</p>
         </div>
-        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+        <div className='flex flex-col gap-4 border border-gray-300 px-6 py-6 text-sm text-gray-500'>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta laboriosam reprehenderit eius accusantium? Aspernatur, nam. Eveniet, quam nesciunt architecto officiis odio tempore at. Incidunt dolore eveniet inventore, harum quisquam optio.</p>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, quos voluptas minima corrupti id magni, delectus fuga at, dignissimos distinctio cum placeat odit iure nobis non quis consequuntur debitis dolor!</p>
         </div>
       </div>
-{/* display related products */}
-
+      {/* display related products */}
+      
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
 
     </div>
   ) : (
